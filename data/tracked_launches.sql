c3ededc466c517273ddc4915387d3bf5eb0b3038
Alex Angelico <alex.angelico@live.com>
2020-11-30 23:22:51 -0800

    updated table dimensions

--- data/tracked_launches.sql
+++ data/tracked_launches.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS tracked_launches;
 
 CREATE TABLE tracked_launches (
   id SERIAL PRIMARY KEY,
-  date VARCHAR (255),
+  data_id VARCHAR (255),
+  launchDate VARCHAR (255),
+  launchTime VARCHAR (255),
   launchProvider TEXT,
   missionName TEXT,
   statusName TEXT,
   missionDescription TEXT,
   orbit TEXT,
-  rocketName TEXT,
-  rocketStartWindow VARCHAR (255),
-  rocketEndWindow VARCHAR (255)
+  rocketName TEXT
 );
